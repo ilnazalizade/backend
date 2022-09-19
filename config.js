147fa925d3d091f919a8d673f2d00f9cbc24b976
Amir <amirsharifico@gmail.com>
2022-09-19 19:41:26 -0400

    Initial commit

---
+++ config.js
@@ -0,0 +1,9 @@
+
+export default {
+  PORT:  5000,
+  MONGODB_URL:  'mongodb://localhost/boobaady',
+  JWT_SECRET:  'somethingsecret',
+  PAYPAL_CLIENT_ID: 'sb',
+  accessKeyId:  'accessKeyId',
+  secretAccessKey:  'secretAccessKey',
+};
