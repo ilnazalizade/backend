33fdf4f63aa01ae35bd495d641712b10290bd0a7
Amir <amirsharifico@gmail.com>
2022-09-20 06:32:27 -0400

    2017

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 export default {
   PORT:  5000,
-  MONGODB_URL:  'mongodb://localhost/boobaady',
+  MONGODB_URL:  'mongodb://localhost:27017/boobaady',
   JWT_SECRET:  'somethingsecret',
   PAYPAL_CLIENT_ID: 'sb',
   accessKeyId:  'accessKeyId',
